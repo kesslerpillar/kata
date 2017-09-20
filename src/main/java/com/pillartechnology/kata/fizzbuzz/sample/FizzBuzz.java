34d674f7f47e142acffd6508443a5f3b3b66bce2
davidkessler-wk <david.kessler@workiva.com>
2017-09-20 18:22:29 -0500

    refactored mod funcionality to a private method for clarity

--- src/main/java/com/pillartechnology/kata/fizzbuzz/sample/FizzBuzz.java
+++ src/main/java/com/pillartechnology/kata/fizzbuzz/sample/FizzBuzz.java
@@ -2,8 +2,12 @@ package com.pillartechnology.kata.fizzbuzz.sample;
 
 public class FizzBuzz {
     public static String convert(int number) {
-        if(number % 3 == 0) return "Fizz";
-        if(number % 5 == 0) return "Buzz";
+        if(isDivisible(number, 3)) return "Fizz";
+        if(isDivisible(number, 5)) return "Buzz";
         return Integer.toString(number);
+    }
+
+    private static boolean isDivisible(int number, int by) {
+        return number % by == 0;
     }
 }
