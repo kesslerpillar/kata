b77bea872031dedd8db9f6f7d273b641a5019e7e
davidkessler-wk <david.kessler@workiva.com>
2017-09-20 17:50:35 -0500

    added baseline for sample starting point

---
+++ src/test/java/com/pillartechnology/kata/fizzbuzz/sample/FizzBuzzTest.java
@@ -0,0 +1,14 @@
+package com.pillartechnology.kata.fizzbuzz.sample;
+
+import com.pillartechnology.kata.fizzbuzz.initial.FizzBuzz;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FizzBuzzTest {
+
+    @Test
+    public void testPlaceholder(){
+        assertEquals("", FizzBuzz.convert(1));
+    }
+}
--- src/test/java/com/pillartechnology/kata/tennis/tennis.md
+++ src/test/java/com/pillartechnology/kata/tennis/tennis.md
@@ -1,6 +1,5 @@
-KataTennis
-
-About this Kata
+Tennis Kata
+===========
 
 This Kata is about implementing a simple tennis game. I came up with it while thinking about Wii tennis, where they have simplified tennis, so each set is one game.
 
