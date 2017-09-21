004e80dbae6f448dee2b43b87fb47485f699c210
davidkessler-wk <david.kessler@workiva.com>
2017-09-21 08:43:07 -0500

    Tennis: added case for player one scoring once

--- src/test/java/com/pillartechnology/kata/tennis/sample/TennisScorerTest.java
+++ src/test/java/com/pillartechnology/kata/tennis/sample/TennisScorerTest.java
@@ -1,14 +1,27 @@
 package com.pillartechnology.kata.tennis.sample;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class TennisScorerTest {
 
+    private TennisGame game;
+
+    @Before
+    public void setUp(){
+        game = new TennisGame("Player 1" , "Player 2");
+    }
+
     @Test
     public void testNewGameShouldReturnLoveAll() {
-        TennisGame game = new TennisGame("Player 1" , "Player 2");
         assertEquals("Love all", game.calculateScore());
+    }
+
+    @Test
+    public void testPlayerOneWinsFirstBall() {
+        game.playerOneScores();
+        assertEquals("Fifteen, Love", game.calculateScore());
     }
 }
