3706b6541a84fd098f4af1e1c17c0a84b421c151
davidkessler-wk <david.kessler@workiva.com>
2017-09-21 09:06:11 -0500

    Tennis: added untracked file

---
+++ src/main/java/com/pillartechnology/kata/tennis/sample/TennisGame.java
@@ -0,0 +1,44 @@
+package com.pillartechnology.kata.tennis.sample;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TennisGame {
+
+    private int playerOneScore;
+    private int playerTwoScore;
+    private static Map<Integer, String> scoreTranslation;
+
+    static{
+        scoreTranslation = Collections.unmodifiableMap(new HashMap<Integer, String>() {
+            {
+                put(0, "Love");
+                put(1, "Fifteen");
+                put(2, "Thirty");
+            }
+        });
+    }
+
+    public TennisGame(String playerOneName, String playerTwoName) {
+        playerOneScore = 0;
+        playerTwoScore = 0;
+    }
+
+    public String calculateScore() {
+        if(playerOneScore == playerTwoScore) return calculateScore(playerOneScore) + " All";
+        return calculateScore(playerOneScore) + ", " + calculateScore(playerTwoScore);
+    }
+
+    public void playerOneScores() {
+        playerOneScore++;
+    }
+
+    public void playerTwoScores() {
+        playerTwoScore++;
+    }
+
+    private String calculateScore(int balls){
+        return scoreTranslation.get(balls);
+    }
+}
