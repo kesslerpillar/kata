447ca72b26b764ceb383f85b168734092a38b6e2
davidkessler-wk <david.kessler@workiva.com>
2017-09-25 09:58:59 -0500

    Roman Numeral: refactor removing duplication - WIP

--- src/main/java/com/pillartechnology/kata/romannumerals/sample/RomanNumerals.java
+++ src/main/java/com/pillartechnology/kata/romannumerals/sample/RomanNumerals.java
@@ -5,54 +5,39 @@ public class RomanNumerals {
     public String convert(int number) {
         Builder romanNumeral = new Builder(number, "");
 
-        romanNumeral = processThousands(romanNumeral);
-        romanNumeral = processHundreds(romanNumeral);
-        romanNumeral = processTens(romanNumeral);
-        romanNumeral = processOnes(romanNumeral);
+        processBase(romanNumeral, new Conversion(1000, "M", 1000));
+
+        append(romanNumeral, new Conversion(900, "CM", 100));
+        append(romanNumeral, new Conversion(500, "D", 100));
+        append(romanNumeral, new Conversion(400, "CD", 100));
+
+        processBase(romanNumeral, new Conversion(100, "C", 100));
+
+        append(romanNumeral, new Conversion(90, "XC", 10));
+        append(romanNumeral, new Conversion(50, "L", 10));
+        append(romanNumeral, new Conversion(40, "XL", 10));
+
+        processBase(romanNumeral, new Conversion(10, "X", 10));
+
+        append(romanNumeral, new Conversion(9, "IX", 1));
+        append(romanNumeral, new Conversion(5, "V", 1));
+        append(romanNumeral, new Conversion(4, "IV", 1));
+
+        processBase(romanNumeral, new Conversion(1, "I", 1));
 
         return romanNumeral.toString();
-    }
-
-    private Builder processOnes(Builder builder) {
-        append(builder, new Conversion(9, "IX", 1));
-        append(builder, new Conversion(5, "V", 1));
-        append(builder, new Conversion(4, "IV", 1));
-
-        processBase(builder, new Conversion(1, "I", 1));
-        return builder;
-    }
-
-    private void processBase(Builder builder, Conversion conversion) {
-        while (builder.count(conversion.number) > 0) {
-            builder.append(conversion.romanNumeral);
-            builder.subtract(conversion.number);
-        }
-    }
-
-    private Builder processTens(Builder builder) {
-        append(builder, new Conversion(90, "XC", 10));
-        append(builder, new Conversion(50, "L", 10));
-        append(builder, new Conversion(40, "XL", 10));
-        processBase(builder, new Conversion(10, "X", 10));
-        return builder;
-    }
-
-    private Builder processHundreds(Builder builder) {
-        append(builder, new Conversion(900, "CM", 100));
-        append(builder, new Conversion(500, "D", 100));
-        append(builder, new Conversion(400, "CD", 100));
-        processBase(builder, new Conversion(100, "C", 100));
-        return builder;
-    }
-
-    private Builder processThousands(Builder builder) {
-        processBase(builder, new Conversion(1000, "M", 1000));
-        return builder;
     }
 
 
     private void append(Builder builder, Conversion conversion) {
         if (builder.count(conversion.range) >= (conversion.number / conversion.range)) {
+            builder.append(conversion.romanNumeral);
+            builder.subtract(conversion.number);
+        }
+    }
+
+    private void processBase(Builder builder, Conversion conversion) {
+        while (builder.count(conversion.number) > 0) {
             builder.append(conversion.romanNumeral);
             builder.subtract(conversion.number);
         }
