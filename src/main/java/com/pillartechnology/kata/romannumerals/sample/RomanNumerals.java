b4efce41b0d331dc1861a3dcda48dbdb19a77a84
davidkessler-wk <david.kessler@workiva.com>
2017-09-25 10:01:23 -0500

    Roman Numeral: refactor removing duplication - WIP

--- src/main/java/com/pillartechnology/kata/romannumerals/sample/RomanNumerals.java
+++ src/main/java/com/pillartechnology/kata/romannumerals/sample/RomanNumerals.java
@@ -5,38 +5,31 @@ public class RomanNumerals {
     public String convert(int number) {
         Builder romanNumeral = new Builder(number, "");
 
-        processBase(romanNumeral, new Conversion(1000, "M", 1000));
+        append(romanNumeral, new Conversion(1000, "M", 1000));
 
         append(romanNumeral, new Conversion(900, "CM", 100));
         append(romanNumeral, new Conversion(500, "D", 100));
         append(romanNumeral, new Conversion(400, "CD", 100));
 
-        processBase(romanNumeral, new Conversion(100, "C", 100));
+        append(romanNumeral, new Conversion(100, "C", 100));
 
         append(romanNumeral, new Conversion(90, "XC", 10));
         append(romanNumeral, new Conversion(50, "L", 10));
         append(romanNumeral, new Conversion(40, "XL", 10));
 
-        processBase(romanNumeral, new Conversion(10, "X", 10));
+        append(romanNumeral, new Conversion(10, "X", 10));
 
         append(romanNumeral, new Conversion(9, "IX", 1));
         append(romanNumeral, new Conversion(5, "V", 1));
         append(romanNumeral, new Conversion(4, "IV", 1));
 
-        processBase(romanNumeral, new Conversion(1, "I", 1));
+        append(romanNumeral, new Conversion(1, "I", 1));
 
         return romanNumeral.toString();
     }
 
 
     private void append(Builder builder, Conversion conversion) {
-        while (builder.count(conversion.range) >= (conversion.number / conversion.range)) {
-            builder.append(conversion.romanNumeral);
-            builder.subtract(conversion.number);
-        }
-    }
-
-    private void processBase(Builder builder, Conversion conversion) {
         while (builder.count(conversion.range) >= (conversion.number / conversion.range)) {
             builder.append(conversion.romanNumeral);
             builder.subtract(conversion.number);
